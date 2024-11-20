7d3effda49918c2eeec1840ed9cefa060a65825e
Douglas Nicida <douglasnicida@gmail.com>
2024-11-20 02:38:06 -0300

    [feat] - expense file delete when creating expense

--- src/components/custom_components/UploadInput.tsx
+++ src/components/custom_components/UploadInput.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "../ui/input";
 import { FormDescription } from "../ui/form";
 import { Trash2Icon } from "lucide-react";
@@ -9,6 +9,7 @@ interface UploadInputProps {
 
 function UploadInput({ onChange }: UploadInputProps) {
   const [previewImages, setPreviewImages] = useState<File[]>([]);
+  const [filesList, setFilesList] = useState<File[]>([]);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
@@ -16,13 +17,31 @@ function UploadInput({ onChange }: UploadInputProps) {
       const fileArray = Array.from(files);
 
       setPreviewImages((prev) => [...prev, ...fileArray])
+      setFilesList([...filesList, ...fileArray])
       onChange(files);
     }
   };
 
   const handleDeleteFile = (fileIndex: number) => {
+    // removendo o item da lista de preview
     setPreviewImages((prev) => prev.filter((file) => file !== previewImages[fileIndex]))
+
+    // removendo item da lista de arquivos
+    setFilesList(filesList.filter((file: File) => file.name !== filesList[fileIndex].name))
   }
+
+  useEffect(() => {
+    // a cada alteração nos arquivos, atualizar o onChange
+    const dataTransfer = new DataTransfer();
+
+    filesList.forEach(file => {
+      dataTransfer.items.add(file);
+    });
+
+    const newFileList: FileList = dataTransfer.files;
+
+    onChange(newFileList)
+  }, [filesList, onChange])
 
   return (
     <>
@@ -41,7 +60,7 @@ function UploadInput({ onChange }: UploadInputProps) {
       <div className="flex flex-wrap gap-4 mt-4">
       {previewImages.map((file, index) => (
         <div key={index} className="relative flex flex-col items-center">
-          <Trash2Icon className="absolute top-1 right-1 cursor-pointer h-6 w-6 hover:scale-110 text-red-500 z-20" onClick={() => {handleDeleteFile(index)}}/>
+          <Trash2Icon className="absolute top-1 right-1 cursor-pointer h-5 w-5 hover:scale-110 text-red-500 z-20" onClick={() => {handleDeleteFile(index)}}/>
           <img
             src={URL.createObjectURL(file)}
             alt={`Preview ${index}`}
--- src/schema/expense.ts
+++ src/schema/expense.ts
@@ -5,7 +5,6 @@ export const ExpenseFormSchema = z.object({
     required_error: "A data de criação é obrigatória",
   }),
   images: z
-    //verificar se aqui é lista de files mesmo ou FileList
     .custom<FileList>()
     .refine((files) => files?.length > 0, "Pelo menos um comprovante é obrigatório")
     .refine(
